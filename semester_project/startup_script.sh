9464358a7bde0d93e8ca333e07f8f42d414ba663
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-11-05 12:19:17 +0000

    Startup_script

--- semester_project/startup_script.sh
+++ semester_project/startup_script.sh
@@ -3,6 +3,8 @@
 sudo apt update -y && sudo apt upgrade -y
 
 #Prerequisites
+#sudo apt install -y wget git apache2 curl
+
 sudo apt install ca-certificates apt-transport-https software-properties-common -y
 
 sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
@@ -20,13 +22,18 @@ sudo apt install php8.1 -y
 
 sudo apt install php8.1-fpm
 
+sudo apt install php8.1-xml
+
+sudo apt install php8.1-zip
+
 #Install a few dependencies
-sudo apt install php8.1-{gd,zip,mysql,oauth,yaml,mbstring,memcache,opcache,xml} unzip -y
+sudo apt install php8.1-{gd,zip,pgsql,oauth,yaml,mbstring,memcache,opcache} unzip -y
 
 sudo apt upgrade -y
 
 #Install composer
 curl -sS https://getcomposer.org/installer | php
 
-#move composer.phar file
+#move composer.phar file and make it executable
 sudo mv composer.phar /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
