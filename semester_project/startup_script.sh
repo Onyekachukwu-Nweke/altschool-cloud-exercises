81e9148e08d7b153880042c0fb3a322c8f40c291
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-11-06 06:05:42 -0800

    Postgre config

--- semester_project/startup_script.sh
+++ semester_project/startup_script.sh
@@ -1,38 +1,10 @@
 #!/usr/bin/env bash
 
-sudo apt update -y && sudo apt upgrade -y
+#Be a root user when running this script
 
-#Prerequisites
-#sudo apt install -y wget git apache2 curl
+sudo apt update && sudo apt upgrade
 
-#apt install ca-certificates apt-transport-https software-properties-common -y
-
-wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
-
-#Adding ppa:ondrej repo
-add-apt-repository -y ppa:ondrej/php
-
-#Update remote server
-apt update -y
-
-#Installation of php8.1
-apt install php8.1 -y
-
-apt install php8.1-fpm
-
-apt install php-xml
-
-apt install php-zip
-
-apt install libapache2-mod-php
-
-#Install a few dependencies
-apt install php-{gd,zip,mysql,oauth,yaml,mbstring,memcache,opcache} unzip -y
-
-apt upgrade -y
-
+sudo apt install postgresql post
 # #Install composer
 # curl -sS https://getcomposer.org/installer | php
 
